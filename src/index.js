70f3b0e71ca0132ba2c83854f8f5bc2f8e0cb27f
Nightlights524 <Nightlights524@users.noreply.github.com>
2020-10-18 01:05:14 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// function Stateless(props) {
-//   return (
-//     <div>
-//     </div>
-//   );
-// }
 
 // const Stateless = props => {
+  //   return (
+    //     <div>
+    //     </div>
+    //   );
+    // }
+    
+// function CalculatorButton(props) {
 //   return (
-//     <div>
-//     </div>
+//     <button>
+//       {props.value}
+//     </button>
 //   );
 // }
 
@@ -20,14 +22,35 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      display: 0
     }
   }
   
   render() {  
     return (
-      <div>
-        <h1>JavaScript Calculator</h1>
+      <div id="container">
+        <div id="calculator">
+          <div id="display">
+            {this.state.display}
+          </div>
+          <button id="clear">AC</button>
+          <button id="divide">/</button>
+          <button id="multiply">x</button>
+          <button id="seven">7</button>
+          <button id="eight">8</button>
+          <button id="nine">9</button>
+          <button id="subtract">-</button>
+          <button id="four">4</button>
+          <button id="five">5</button>
+          <button id="six">6</button>
+          <button id="add">+</button>
+          <button id="one">1</button>
+          <button id="two">2</button>
+          <button id="three">3</button>
+          <button id="equals">=</button>
+          <button id="zero">0</button>
+          <button id="decimal">.</button>
+        </div>
       </div>
     );
   }
